83113167d027afe3a8c540a4cd422384feab8985
Venkatesh <venkateshthedeveloper@gmail.com>
2025-03-29 15:54:28 +0530

    Initial Changes

---
+++ src/main/java/com/venya/lend_manager/LendManagerApplication.java
@@ -0,0 +1,13 @@
+package com.venya.lend_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venya/lend_manager/LendManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venya.lend_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
